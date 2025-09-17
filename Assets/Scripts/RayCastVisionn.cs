e7249fd56ba2482e29869344ac18c764a937280d
Agustin Hofmann <agustinhofmann@gmail.com>
2025-09-17 08:58:55 -0300

    Initial commit

---
+++ Assets/Scripts/RayCastVisionn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastVisionn : MonoBehaviour
+
+{
+
+    [SerializeField] Vector3 rayOrigin;
+    [SerializeField] Transform sightOrigin;
+    [SerializeField] float rayDistance;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(sightOrigin.position, sightOrigin.forward, out hitInfo, rayDistance))
+        {
+
+            Debug.Log(hitInfo.collider.gameObject);
+
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Color color = Color.red;
+        Gizmos.DrawLine(sightOrigin.position, sightOrigin.position + sightOrigin.forward * rayDistance);
+    }
+}
